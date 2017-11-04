017683c9c0a00fbcdcfd930ee61b0a46fb68b370
ricardo.millauro <ricardo.millauro@bbva.com>
2017-11-04 11:17:30 -0300

    Se agrega geolocalizacion en el back

--- common/models/cajero.js
+++ common/models/cajero.js
@@ -1,5 +1,45 @@
 'use strict';
 
-module.exports = function(Cajero) {
+var connect = require('connect');
+var http = require('http');
+var loopback = require('loopback');
 
+const geolocation = require('google-geolocation')({
+  key: 'AIzaSyA27HjYihRX4mTIaYmO2JF98amfNMIk8L0',
+});
+
+const params = {
+  wifiAccessPoints: [
+    {
+      macAddress: '01:23:45:67:89:AB',
+      signalStrength: -65,
+      signalToNoiseRatio: 40,
+    },
+  ],
+};
+
+module.exports = function(Cajero) {
+  Cajero.localizacion = function(cb) {
+    geolocation(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(data);
+      cb(data);
+    });
+  };
+
+  Cajero.remoteMethod(
+    'localizacion', {
+      http: {
+        path: '/localizacion',
+        verb: 'get',
+      },
+      returns: {
+        arg: 'localizacion',
+        type: 'string',
+      },
+    }
+  );
 };
