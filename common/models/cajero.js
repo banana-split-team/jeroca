5ee98dfd87ee208e7bcd37d55522edd98d4dd90f
ricardo.millauro <ricardo.millauro@bbva.com>
2017-11-05 11:11:07 -0300

    Se incorpora API de visa, y se formatea .

--- common/models/cajero.js
+++ common/models/cajero.js
@@ -1,43 +1,123 @@
 'use strict';
 
-var connect = require('connect');
-var http = require('http');
-var loopback = require('loopback');
+var request = require('request');
+var fs = require('fs');
+var VisaAPIClient = require('../../server/visa/visaapiclient.js');
+var config = require('../../server/config/configuration.json');
 
-const geolocation = require('google-geolocation')({
-  key: 'AIzaSyA27HjYihRX4mTIaYmO2JF98amfNMIk8L0',
+var req = request.defaults();
+var userId = config.userId;
+var password = config.password;
+var keyFile = config.key;
+var certificateFile = config.cert;
+
+var visaAPIClient = new VisaAPIClient();
+var strDate = new Date().toISOString();
+var atmInquiryRequest = JSON.stringify({
+  'requestData': {
+    'culture': 'en-US',
+    'distance': '2000',
+    'distanceUnit': 'mi',
+    'location': {
+      'address': null,
+      'geocodes': {
+        'longitude': -48.60609132008005,
+        'latitude': -28.844737680202572,
+      },
+      'placeName': null,
+    },
+    'metaDataOptions': 0,
+    'options': {
+      'findFilters': [{
+        'filterName': 'PLACE_NAME',
+        'filterValue': 'FORT FINANCIAL CREDIT UNION|ULTRON INC|U.S. BANK',
+      },
+      {
+        'filterName': 'OPER_HRS',
+        'filterValue': 'C',
+      },
+      {
+        'filterName': 'AIRPORT_CD',
+        'filterValue': '',
+      },
+      {
+        'filterName': 'WHEELCHAIR',
+        'filterValue': 'N',
+      },
+      {
+        'filterName': 'BRAILLE_AUDIO',
+        'filterValue': 'N',
+      },
+      {
+        'filterName': 'BALANCE_INQUIRY',
+        'filterValue': 'N',
+      },
+      {
+        'filterName': 'CHIP_CAPABLE',
+        'filterValue': 'N',
+      },
+      {
+        'filterName': 'PIN_CHANGE',
+        'filterValue': 'N',
+      },
+      {
+        'filterName': 'RESTRICTED',
+        'filterValue': 'N',
+      },
+      {
+        'filterName': 'PLUS_ALLIANCE_NO_SURCHARGE_FEE',
+        'filterValue': 'N',
+      },
+      {
+        'filterName': 'ACCEPTS_PLUS_SHARED_DEPOSIT',
+        'filterValue': 'N',
+      },
+      {
+        'filterName': 'V_PAY_CAPABLE',
+        'filterValue': 'N',
+      },
+      {
+        'filterName': 'READY_LINK',
+        'filterValue': 'N',
+      },
+      ],
+      'operationName': 'or',
+      'range': {
+        'count': 99,
+        'start': 0,
+      },
+      'sort': {
+        'direction': 'desc',
+        'primary': 'city',
+      },
+      'useFirstAmbiguous': true,
+    },
+  },
+  'wsRequestHeaderV2': {
+    'applicationId': 'VATMLOC',
+    'correlationId': '909420141104053819418',
+    'requestMessageId': 'test12345678',
+    'requestTs': strDate,
+    'userBid': '10000108',
+    'userId': 'CDISIUserID',
+  },
 });
 
-const params = {
-  wifiAccessPoints: [
-    {
-      macAddress: '01:23:45:67:89:AB',
-      signalStrength: -65,
-      signalToNoiseRatio: 40,
-    },
-  ],
-};
-
 module.exports = function(Cajero) {
-  Cajero.localizacion = function(cb) {
-    geolocation(params, (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(data);
-      cb(data);
-    });
+  Cajero.listado = function(cb) {
+    var baseUri = 'globalatmlocator/';
+    var resourcePath = 'v1/localatms/atmsinquiry';
+    visaAPIClient.doMutualAuthRequest(baseUri + resourcePath, atmInquiryRequest, 'POST', {}, cb);
   };
 
   Cajero.remoteMethod(
-    'localizacion', {
+    'listado', {
       http: {
-        path: '/localizacion',
+        path: '/listado',
         verb: 'get',
       },
       returns: {
-        arg: 'localizacion',
+        arg: 'listado',
         type: 'string',
       },
     }
